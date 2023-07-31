d06f52d03949ffda280e477c3569c1b58607e1fe
eudes-silva <eudes-silva@tutanota.com>
2023-07-31 07:34:03 -0300

    feat: add CoreBtn.vue component

---
+++ components/CoreBtn.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-btn
+    @click="handleClickEvent"
+    :class="propClass"
+    :color="color"
+    :variant="variant"
+    :size="size"
+    :rounded="rounded"
+    :prepend-icon="prependIcon"
+    :append-icon="appendIcon"
+  >
+    {{ label }}
+  </v-btn>
+</template>
+<script setup lang="ts">
+import { computed } from "vue";
+
+const emit = defineEmits(["clickEvent"]);
+const props = withDefaults(
+  defineProps<{
+    kind?: "primary" | "secondary" | "tertiary";
+    label: string;
+    color?: string;
+    rounded?: string;
+    size?: string;
+    propClass?: string;
+    propIcon?: string;
+    prependIcon?: string;
+    appendIcon?: string;
+  }>(),
+  {
+    kind: "primary",
+    label: "",
+    color: "",
+    rounded: "",
+    size: "",
+    propClass: "",
+    prependIcon: "",
+    appendIco: "",
+  }
+);
+const variant = computed(
+  () =>
+    ((
+      {
+        primary: "elevated",
+        secondary: "outlined",
+        tertiary: "flat",
+      } as const
+    )[props.kind])
+);
+
+function handleClickEvent() {
+  emit("clickEvent");
+}
+</script>
