8befadb6d626bc66a3b8269c13275c0950c7a0a5
eudes-silva <eudes-silva@tutanota.com>
2023-07-31 07:36:27 -0300

    feat: add DrinksList.vue component

---
+++ components/DrinksList.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th
+          class="text-primary text-left"
+          v-for="header in headers"
+          :key="header.id"
+        >
+          {{ header.name }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in props.items" :key="item.idDrink">
+        <td
+          @click="header.value === 'strDrink' ? handleClickEmit(item) : ''"
+          class="text-primary"
+          v-for="header in props.headers"
+        >
+          {{ item[header.value]
+          }}<v-icon
+            class="ml-2 mb-1 details-icon"
+            v-if="header.value === 'strDrink'"
+            color="secondary"
+            small
+            >mdi-eye-outline</v-icon
+          >
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+<script setup lang="ts">
+const emit = defineEmits(["getDrinkDetailsByName"]);
+import { reactive, computed } from "vue";
+
+const state = reactive({
+  itemId: null,
+});
+
+const props = withDefaults(
+  defineProps<{
+    headers?: Array<any>;
+    items?: Array<any>;
+  }>(),
+  {
+    headers: () => [],
+    items: () => [],
+  }
+);
+
+function handleClickEmit(drinkName: object) {
+  emit("getDrinkDetailsByName", drinkName);
+}
+function showDetailsBtn(id: any) {
+  state.itemId = id;
+}
+</script>
+<style lang="scss" scoped>
+.details-icon {
+  cursor: pointer;
+}
+</style>
