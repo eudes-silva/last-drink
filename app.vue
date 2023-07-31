3fe79e536fa44c9a9d3cda0115dd7a9483ba272b
eudes-silva <eudes-silva@tutanota.com>
2023-07-31 07:28:53 -0300

    Add initial setup for app.vue and error.vue pages

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-screen">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ error.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="text-center h-screen error-container">
+    <div>
+      <v-icon size="x-large" color="warning">mdi-alert</v-icon>
+      <h1>{{ error.statusCode }}</h1>
+      <p>{{ error.message }}</p>
+      <v-btn
+        color="primary"
+        size="small"
+        class="mt-2 text-lowercase"
+        @click="handleError"
+        >Voltar para a página inicial</v-btn
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  error: Object,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
+<style lang="scss">
+html {
+  height: 100vh;
+}
+.error-container {
+  display: grid;
+  place-items: center;
+}
+</style>
