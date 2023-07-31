39594df180145e06677000eca6b107b2d68de69b
eudes-silva <eudes-silva@tutanota.com>
2023-07-31 07:35:42 -0300

    feat: add CoreModal.vue component

---
+++ components/CoreModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="state.dialog" width="auto">
+      <div v-if="$slots.content">
+        <slot name="content" />
+      </div>
+    </v-dialog>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from "vue";
+const props = withDefaults(
+  defineProps<{
+    dialog: boolean;
+  }>(),
+  {
+    dialog: false,
+  }
+);
+const state = reactive({
+  dialog: props.dialog,
+});
+</script>
