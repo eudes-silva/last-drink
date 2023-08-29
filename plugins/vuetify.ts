71d4ee45ec9b224038dd03c6bb7b6cedd0464444
eudes-silva <eudes-silva@tutanota.com>
2023-08-29 04:55:37 -0300

    fix: themedefinition unused remove

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,4 +1,4 @@
-import { createVuetify, ThemeDefinition } from "vuetify";
+import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
