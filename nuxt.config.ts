18d3ce8017a5b360a729e85a02acc7e5be0896e9
eudes-silva <eudes-silva@tutanota.com>
2023-08-31 21:40:59 -0300

    feat: runtimeconfig apibase + apikey from .env add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,12 @@ import VueI18nVitePlugin from "@intlify/unplugin-vue-i18n/vite";
 export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
+  runtimeConfig: {
+    // Keys within public, will be also exposed to the client-side
+    public: {
+      apiBase: `/api/json/v1/${process.env.API_KEY}`,
+    },
+  },
   css: [
     "vuetify/lib/styles/main.sass",
     "@mdi/font/css/materialdesignicons.css",
